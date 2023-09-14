59432d931cdb11c7da895372c09ba821ab06a57e
Algerina <kh.amelle@gmail.com>
2023-09-14 22:30:05 +0100

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Hero from "./components/Hero.jsx";
+import Demo from "./components/Demo.jsx";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
 };
 
 export default App;
