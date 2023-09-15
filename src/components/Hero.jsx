eb3d3e885d50b8b220cb51451f180d964f3216cb
Algerina <kh.amelle@gmail.com>
2023-09-15 16:20:14 +0100

    Add Navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center item-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/algerina")}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+    </header>
+  );
 };
 
 export default Hero;
