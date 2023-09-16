93ca2ca7ed4666e4b31d1bc06fe9384e42722d2e
Algerina <kh.amelle@gmail.com>
2023-09-16 17:06:38 +0100

    Add article's history display section

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -70,6 +70,26 @@ const Demo = () => {
         </form>
 
         {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={"link-${index}"}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h=[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Results */}
