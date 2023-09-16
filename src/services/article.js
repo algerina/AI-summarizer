d17487c940972e36921e974e78e5f9133cd1c9ec
Algerina <kh.amelle@gmail.com>
2023-09-16 16:05:30 +0100

    Fix API response issue

--- src/services/article.js
+++ src/services/article.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
