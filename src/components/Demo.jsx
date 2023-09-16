738ab2da85b72ee88820166ecbc1d3c4cb6f133c
Algerina <kh.amelle@gmail.com>
2023-09-16 15:36:37 +0100

    Save articles in local storage

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -12,6 +12,15 @@ const Demo = () => {
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { data } = await getSummary({ articleUrl: article.url });
