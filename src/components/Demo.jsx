af3a6263702949d3e377d2d38f42bf94372aed03
Algerina <kh.amelle@gmail.com>
2023-09-20 17:47:55 +0100

    Add copy link functionality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,7 +9,7 @@ const Demo = () => {
     summary: "",
   });
   const [allArticles, setAllArticles] = useState([]);
-
+  const [copied, setCopied] = useState("");
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
@@ -33,13 +33,17 @@ const Demo = () => {
       setAllArticles(updatedAllArticles);
 
       localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-
-      console.log(newArticle);
     }
   };
 
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
   return (
-    <section>
+    <section className="mt-16 w-full max-w-xl">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form
@@ -77,9 +81,9 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className="link_card"
             >
-              <div className="copy_btn">
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                 <img
-                  src={copy}
+                  src={copied === item.url ? tick : copy}
                   alt="copy_icon"
                   className="w-[40%] h=[40%] object-contain"
                 />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,9 @@ const Hero = () => {
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
         <button
           type="button"
-          onClick={() => window.open("https://github.com/algerina")}
+          onClick={() =>
+            window.open("https://github.com/algerina/AI-summarizer", "_blank")
+          }
           className="black_btn"
         >
           Github
